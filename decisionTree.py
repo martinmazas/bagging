3cef47eb7a4af8230aa028dfe543a250ca90b368
martinmazas <mazasmartin@gmail.com>
2021-01-24 17:19:38 +0200

    getting prediction results of each data set

--- decisionTree.py
+++ decisionTree.py
@@ -1,4 +1,5 @@
 from sklearn import tree
+from sklearn.metrics import accuracy_score
 import numpy as np
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
@@ -8,30 +9,46 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
 
+def convert_columns_into_integers_values(dataframe):
+    dataframe.pclass = dataframe.pclass.map({'crew': 0, '1st': 1, '2nd': 2, '3rd': 3})
+    dataframe.gender = dataframe.gender.map({'male': 1, 'female': 0})
+    dataframe.age = dataframe.age.map({'adult': 1, 'child': 0})
+    dataframe.survived = dataframe.survived.map({'yes': 1, 'no': 0})
+    return dataframe
+
+
 def treeCreate():
-    # Save original data
+    # Save original data into data frames
     titanic = pd.read_csv("titanikData.csv")
+    header = titanic.columns.values
+    titanic_test = pd.read_csv("titanikTest.csv", names=header)
+
+    titanic_test = convert_columns_into_integers_values(titanic_test)
+    titanic_training_unique_values = titanic.drop_duplicates()
+
 
     # Preparing and saving data test
-    titanic_test_header = pd.DataFrame(columns=['pclass','age','gender'])
-    titanic_rows= pd.read_csv("titanikTest.csv")
-    titanic_test = pd.concat([titanic_test_header, titanic_rows])
-    titanic_test.columns.insert(0,['pclass','age','gender'])
+    # titanic_test_header = pd.DataFrame(columns=['pclass', 'age', 'gender'])
+    # titanic_rows = pd.read_csv("titanikTest.csv")
+    # print(titanic_rows)
+    # titanic_test = pd.concat([titanic_test_header, titanic_rows])
+    # print(titanic_test)
+    # titanic_test.columns.insert(0, ['pclass', 'age', 'gender'])
 
     # convert string data into integer
-    titanic_input = titanic
-    titanic_input.gender = titanic.gender.map({'male': 1, 'female': 0})
-    titanic_input.age = titanic.age.map({'adult': 1, 'child': 0})
-    titanic_input.pclass = titanic.pclass.map({'crew': 0, '1st': 1, '2nd': 2, '3rd': 3})
-    titanic_input.survived = titanic.survived.map({'yes': 1, 'no': 0})
-
-    titanic_test['pclass'] = titanic_test['1st'].map({'crew': 0, '1st': 1, '2nd': 2, '3rd': 3})
-    titanic_test['age'] = titanic_test.adult.map({'adult': 1, 'child': 0})
-    titanic_test['gender'] = titanic_test.male.map({'male': 1, 'female': 0})
-    titanic_test.drop(['1st', 'adult', 'male', 'yes'], axis=1, inplace=True)
+    # titanic_input = titanic
+    # titanic_input.gender = titanic.gender.map({'male': 1, 'female': 0})
+    # titanic_input.age = titanic.age.map({'adult': 1, 'child': 0})
+    # titanic_input.pclass = titanic.pclass.map({'crew': 0, '1st': 1, '2nd': 2, '3rd': 3})
+    # titanic_input.survived = titanic.survived.map({'yes': 1, 'no': 0})
+    #
+    # titanic_test['pclass'] = titanic_test['1st'].map({'crew': 0, '1st': 1, '2nd': 2, '3rd': 3})
+    # titanic_test['age'] = titanic_test.adult.map({'adult': 1, 'child': 0})
+    # titanic_test['gender'] = titanic_test.male.map({'male': 1, 'female': 0})
+    # titanic_test.drop(['1st', 'adult', 'male', 'yes'], axis=1, inplace=True)
 
     # save unique inputs
-    titanic_unique_input = titanic_input.drop_duplicates()
+    # titanic_unique_input = titanic_input.drop_duplicates()
 
     # for i in titanic_unique_input.values:
     #     print(i)
@@ -51,11 +68,12 @@ def treeCreate():
     #     # print([i[0], i[1], i[2]], clf.predict([[i[0], i[1], i[2]]]))
     # # print(test.unstack().head())
 
-    predictions = list()
+    # predictions = list()
+    titanic_test_input = titanic_test.drop('survived', axis=1)
 
     # Create 100 dataset of size n. 65% are without replacement the other 35% with replacement
-    for i in range(100):
-        titanic_list_without_replacement = titanic_input.sample(frac=.632)
+    for i in range(3):
+        titanic_list_without_replacement = titanic_training_unique_values.sample(frac=.632)
         titanic_size = len(titanic)
         titanic_list_without_replacement_size = len(titanic_list_without_replacement)
         complement_list_size = titanic_size - titanic_list_without_replacement_size
@@ -63,15 +81,25 @@ def treeCreate():
 
         t_list = [titanic_list_without_replacement, titanic_list_with_replacement]
         titanic_list = pd.concat(t_list)
-        titanic_target = titanic_list['survived']
-        titanic_list = titanic_list.drop('survived', axis=1)
+        titanic_list = convert_columns_into_integers_values(titanic_list)
+
+        features = ['pclass', 'age', 'gender']
+        x = titanic_list[features]
+        y = titanic_list.survived
+        # print(y)
+
+        # titanic_target = titanic_list['survived']
+        # titanic_list = titanic_list.drop('survived', axis=1)
 
         clf = tree.DecisionTreeClassifier()
         clf.max_depth = 2
         clf.criterion = 'entropy'
-        clf = clf.fit(titanic_list, titanic_target)
-        prediction = clf.predict(titanic_test)
+        clf = clf.fit(x, y)
+
+        prediction = clf.predict(titanic_test_input)
         print(prediction)
+        # print(prediction)
+        print("accuracy " + str(i) + "= " + str(accuracy_score(titanic_test.survived, prediction, normalize=False)))
         # predictions.append(prediction)
 
     # array_of_predictions = np.array(predictions)
